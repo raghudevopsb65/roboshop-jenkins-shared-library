5c2f485bd96f5e10f8bf70f0a154b0fd646d8e1f
Raghu K <raghuk.vit@gmail.com>
2022-07-06 06:37:59 +0530

    Update roboshop-jenkins-shared-library/

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,6 +1,6 @@
 def pipelineInit() {
   stage('Initiate Repo') {
     sh 'rm -rf *'
-    git branch: 'main', url: 'https://github.com/raghudevopsb65/cart.git'
+    git branch: 'main', url: "https://github.com/raghudevopsb65/${COMPONENT}.git"
   }
 }
