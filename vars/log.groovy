e8f459c3f9867f4a9b7fa2075b1beb435c473a67
Raghu K <raghuk.vit@gmail.com>
2022-07-06 06:22:51 +0530

    Update roboshop-jenkins-shared-library/

--- vars/log.groovy
+++ vars/log.groovy
@@ -6,20 +6,38 @@ def warning(message) {
   echo "WARNING: ${message}"
 }
 
+//def call() {
+//  pipeline {
+//    agent any
+//    stages {
+//      stage('One') {
+//        steps {
+//          sh 'echo One - ${COMPONENT}'
+//        }
+//      }
+//      stage('Two') {
+//        steps {
+//          sh 'echo Two'
+//        }
+//      }
+//    }
+//  }
+//}
+
 def call() {
-  pipeline {
-    agent any
-    stages {
-      stage('One') {
-        steps {
-          sh 'echo One - ${COMPONENT}'
-        }
-      }
-      stage('Two') {
-        steps {
-          sh 'echo Two'
-        }
-      }
+  node() {
+
+    stage('One') {
+      sh 'echo One - ${COMPONENT}'
     }
+
+    ///
+    //
+
+    stage('Two') {
+      sh 'echo Two - ${COMPONENT}'
+    }
+    
   }
 }
+
