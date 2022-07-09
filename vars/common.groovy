aae2af721adf98387608e329581b4ab99c6ca0be
Raghu K <raghuk.vit@gmail.com>
2022-07-09 06:43:05 +0530

    Update roboshop-jenkins-shared-library/

--- vars/common.groovy
+++ vars/common.groovy
@@ -6,27 +6,28 @@ def pipelineInit() {
 }
 
 def publishArtifacts() {
+  var.ENV = "dev"
   stage("Prepare Artifacts"){
     if (env.APP_TYPE == "nodejs") {
       sh """
-        zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js
+        zip -r ${ENV}-${COMPONENT}-${TAG_NAME}.zip node_modules server.js
       """
     }
     if (env.APP_TYPE == "maven") {
       sh """
         cp target/${COMPONENT}-1.0.jar ${COMPONENT}.jar
-        zip -r ${COMPONENT}-${TAG_NAME}.zip ${COMPONENT}.jar
+        zip -r ${ENV}-${COMPONENT}-${TAG_NAME}.zip ${COMPONENT}.jar
       """
     }
     if (env.APP_TYPE == "python") {
       sh """
-        zip -r ${COMPONENT}-${TAG_NAME}.zip *.py ${COMPONENT}.ini requirements.txt
+        zip -r ${ENV}-${COMPONENT}-${TAG_NAME}.zip *.py ${COMPONENT}.ini requirements.txt
       """
     }
     if (env.APP_TYPE == "nginx") {
       sh """
         cd static
-        zip -r ../${COMPONENT}-${TAG_NAME}.zip *
+        zip -r ../${ENV}-${COMPONENT}-${TAG_NAME}.zip *
       """
     }
   }
@@ -34,7 +35,7 @@ def publishArtifacts() {
   stage('Push Artifacts to Nexus') {
     withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'pass', usernameVariable: 'user')]) {
       sh """
-        curl -v -u ${user}:${pass} --upload-file dev-${COMPONENT}-${TAG_NAME}.zip http://172.31.7.89:8081/repository/${COMPONENT}/dev-${COMPONENT}-${TAG_NAME}.zip
+        curl -v -u ${user}:${pass} --upload-file ${ENV}-${COMPONENT}-${TAG_NAME}.zip http://172.31.7.89:8081/repository/${COMPONENT}/${ENV}-${COMPONENT}-${TAG_NAME}.zip
       """
     }
   }
