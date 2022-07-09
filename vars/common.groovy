89acd18950cb16da763c536d0fec646c87bc17a1
Raghu K <raghuk.vit@gmail.com>
2022-07-09 07:30:22 +0530

    Update roboshop-jenkins-shared-library/

--- vars/common.groovy
+++ vars/common.groovy
@@ -55,7 +55,7 @@ def publishArtifacts() {
 def promoteRelease(SOURCE_ENV,ENV) {
   withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'pass', usernameVariable: 'user')]) {
     sh """
-      cp ${SOURCE_ENV}-${COMPONENT}-${TAG_NAME}.zip cp ${ENV}-${COMPONENT}-${TAG_NAME}.zip
+      cp ${SOURCE_ENV}-${COMPONENT}-${TAG_NAME}.zip ${ENV}-${COMPONENT}-${TAG_NAME}.zip
       curl -v -u ${user}:${pass} --upload-file ${ENV}-${COMPONENT}-${TAG_NAME}.zip http://172.31.7.89:8081/repository/${COMPONENT}/${ENV}-${COMPONENT}-${TAG_NAME}.zip
     """
   }
