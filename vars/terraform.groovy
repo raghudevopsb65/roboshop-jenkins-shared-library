1da236027ec3ecfdfe86c143cfadafb812131b54
Raghu K <raghuk.vit@gmail.com>
2022-07-28 06:12:10 +0530

    Update roboshop-jenkins-shared-library/

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,17 +1,17 @@
 def call() {
   node {
+    ansiColor('xterm') {
+      stage('Terraform INIT') {
+        sh 'terraform init'
+      }
 
-    stage('Terraform INIT') {
-      sh 'terraform init'
+      stage('Terraform Plan') {
+        sh 'terraform plan'
+      }
+
+      stage('Terraform Apply') {
+        sh 'terraform apply -auto-approve'
+      }
     }
-
-    stage('Terraform Plan') {
-      sh 'terraform plan'
-    }
-
-    stage('Terraform Apply') {
-      sh 'terraform apply -auto-approve'
-    }
-
   }
 }
