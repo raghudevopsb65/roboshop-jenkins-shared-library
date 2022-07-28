fca4b15427c775362194f3c896e3112d97aa1ad6
Raghu K <raghuk.vit@gmail.com>
2022-07-28 07:22:39 +0530

    Update roboshop-jenkins-shared-library/

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -3,6 +3,7 @@ def call() {
     properties([
         parameters([
             choice(choices: ['dev', 'prod'], description: "Choose Environment", name: "ENV"),
+            choice(choices: ['apply', 'destroy'], description: "Choose Action", name: "ACTION"),
         ])
     ])
 
@@ -21,8 +22,8 @@ def call() {
         sh 'terraform plan -var-file=env/${ENV}.tfvars'
       }
 
-      stage('Terraform Apply') {
-        sh 'terraform apply -auto-approve -var-file=env/${ENV}.tfvars'
+      stage("Terraform ${ACTION}") {
+        sh 'terraform ${ACTION} -auto-approve -var-file=env/${ENV}.tfvars'
       }
     }
   }
