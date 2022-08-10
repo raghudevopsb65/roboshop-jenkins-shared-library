d2253b4490194a4285b36d14df3731bea1c969d2
Raghu K <raghuk.vit@gmail.com>
2022-08-10 06:16:37 +0530

    Update roboshop-jenkins-shared-library/

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -3,6 +3,10 @@ def call() {
   node() {
 
     common.pipelineInit()
+    if( env.BRANCH_NAME == env.TAG_NAME )
+    {
+      sh 'git checkout ${TAG_NAME}'
+    }
 
     stage('Build Package') {
       sh '''
@@ -14,7 +18,10 @@ def call() {
 
     if( env.BRANCH_NAME == env.TAG_NAME )
     {
-      common.publishArtifacts()
+      //common.publishArtifacts()
+      common.publishLocalArtifacts()
+      // This is added for Immutable Approach
+      common.publishAMI()
     }
 
   }
--- vars/python.groovy
+++ vars/python.groovy
@@ -3,11 +3,18 @@ def call() {
   node() {
 
     common.pipelineInit()
+    if( env.BRANCH_NAME == env.TAG_NAME )
+    {
+      sh 'git checkout ${TAG_NAME}'
+    }
     common.codeChecks()
 
     if( env.BRANCH_NAME == env.TAG_NAME )
     {
-      common.publishArtifacts()
+      //common.publishArtifacts()
+      common.publishLocalArtifacts()
+      // This is added for Immutable Approach
+      common.publishAMI()
     }
 
   }
